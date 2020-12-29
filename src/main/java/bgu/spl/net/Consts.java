44582d667d8c52f22fe3ebd96470ebd2fcf562d0
yonathan95 <yonathan.wolloch@gmail.com>
2020-12-29 20:40:00 +0200

    Database

---
+++ src/main/java/bgu/spl/net/Consts.java
@@ -0,0 +1,17 @@
+package bgu.spl.net;
+
+public class Consts {
+    public static final int NOT_REGISTERED = 0;
+    public static final int REGISTERED_ADMINISTRATOR_SUCCESSFULLY = 1;
+    public static final int REGISTERED_STUDENT_SUCCESSFULLY = 2;
+    public static final int WRONG_PASSWORD = 3;
+    public static final int IS_LOGGED_IN = 4;
+    public static final int NOT_LOGGED_IN = 5;
+    public static final int DONT_HAVE_KDAMS = 6;
+    public static final int HAVE_KDAMS = 7;
+    public static final int COURSE_IS_FULL = 8;
+    public static final int NO_SUCH_COURSE = 9;
+    public static final int REGISTERED_COURSE_SUCCESSFULLY = 10;
+    public static final int LOGGED_OUT_SUCCESSFULLY = 11;
+    public static final int IS_REGISTERED = 12;
+}
--- src/main/java/bgu/spl/net/Course.java
+++ src/main/java/bgu/spl/net/Course.java
@@ -1,14 +1,50 @@
 package bgu.spl.net;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.SortedSet;
+import java.util.*;
 
 public class Course {
-    private int courseNum;
-    private String courseName;
-    private ArrayList KdamCoursesList;
+    private final int courseNum;
+    private final String courseName;
+    private final int[] kdamCoursesList;
     private int numOfMaxStudents;
-    private SortedSet registeredStudents;
+    private TreeSet<String> registeredStudents;
+
+    public Course(int _courseNum, String _courseName, int[] _kdamCoursesList, int _numOfMaxStudents){
+        courseNum = _courseNum;
+        courseName = _courseName;
+        kdamCoursesList = _kdamCoursesList;
+        numOfMaxStudents = _numOfMaxStudents;
+        registeredStudents = new TreeSet<>();
+    }
+
+    public void registerStudent(String studentUsername){
+        registeredStudents.add(studentUsername);
+    }
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int[] getKdamCoursesList() {
+        return kdamCoursesList;
+    }
+
+    public int getNumOfMaxStudents() {
+        return numOfMaxStudents;
+    }
+
+    public void setNumOfMaxStudents(int numOfMaxStudents) {
+        this.numOfMaxStudents = numOfMaxStudents;
+    }
+
+    public TreeSet<String> getRegisteredStudents() {
+        return registeredStudents;
+    }
+
+    public void setRegisteredStudents(TreeSet<String> registeredStudents) {
+        this.registeredStudents = registeredStudents;
+    }
 }
