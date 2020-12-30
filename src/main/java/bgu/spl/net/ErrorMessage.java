04c9274199361eab48cd5312ac6b93deafa60722
eladweinfeld <eladweinfeld@gmail.com>
2020-12-30 18:07:13 +0200

    Message has been crated

--- src/main/java/bgu/spl/net/ErrorMessage.java
+++ src/main/java/bgu/spl/net/ErrorMessage.java
@@ -4,11 +4,16 @@ import bgu.spl.net.api.OpMessage;
 
 public class ErrorMessage implements OpMessage<Short> {
     private Short Opcode = 13;
+
+
+
     private short MessageOpcode;
     public ErrorMessage(short _MessageOpcode){
         MessageOpcode = _MessageOpcode;
     }
-
+    public void setMessageOpcode(short messageOpcode) {
+        MessageOpcode = messageOpcode;
+    }
     public short getMessageOpcode() {
         return MessageOpcode;
     }
--- src/main/java/bgu/spl/net/LogoutMyCoursesMessages.java
+++ src/main/java/bgu/spl/net/LogoutMyCoursesMessages.java
@@ -3,12 +3,12 @@ package bgu.spl.net;
 import bgu.spl.net.api.OpMessage;
 
 public class LogoutMyCoursesMessages implements OpMessage<Short> {
-    private short Opcode = -1;
+    private Short Opcode = -1;
 
     public LogoutMyCoursesMessages(short _Opcode){
         Opcode = _Opcode;
     }
-   public Short getOpcode(){
+    public Short getOpcode(){
         return Opcode;
     }
 }
