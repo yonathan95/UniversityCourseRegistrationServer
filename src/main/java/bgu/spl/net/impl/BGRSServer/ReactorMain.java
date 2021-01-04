9a114a6ece088a6daeb9847c0bb88bf4b02f485c
yonathan95 <yonathan.wolloch@gmail.com>
2021-01-04 14:09:00 +0200

    Database

--- src/main/java/bgu/spl/net/impl/BGRSServer/ReactorMain.java
+++ src/main/java/bgu/spl/net/impl/BGRSServer/ReactorMain.java
@@ -4,10 +4,12 @@ import bgu.spl.net.impl.BGRSServer.srv.Server;
 public class ReactorMain {
     public static void main(String[] args) {
         Database database = Database.getInstance();
+        int port = Integer.parseInt(args[0]);
+        int threadNum = Integer.parseInt(args[1]);
         database.initialize("./Courses.txt");
         Server.reactor(
-                Integer.getInteger(args[1]),
-                (Integer.getInteger(args[0])), //port
+                threadNum,
+                port, //port
                 () ->  new MessagingProtocolImpl(database), //protocol factory
                 () -> new MessageEncoderDecoderImpl<>()//message encoder decoder factory
         ).serve();
